3b528547e7c0c67d528436621623f6a2f2c272ac
Cleverson Biora <cleverson.biora@gmail.com>
2019-04-28 22:46:17 -0300

    Publish

---
+++ build/precache-manifest.1410ef05419f447d5148a14a306e51ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162b96294926165eb99e5b5421c3743a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3e31794d53ac804881",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4f68c555897d3f8394da",
+    "url": "/static/css/main.f023d3f6.chunk.css"
+  },
+  {
+    "revision": "8c3e31794d53ac804881",
+    "url": "/static/js/2.84f14576.chunk.js"
+  },
+  {
+    "revision": "4f68c555897d3f8394da",
+    "url": "/static/js/main.bf00ce47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
